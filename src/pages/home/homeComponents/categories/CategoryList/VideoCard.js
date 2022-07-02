de8b62c81467f910fb670fbbc7bd8822b7b5fcfa
vashnavichauhan1825 <vashnavichauhan1@gmail.com>
2022-07-02 23:35:56 +0530

    add functionality to sidebar cards

--- src/pages/home/homeComponents/categories/CategoryList/VideoCard.js
+++ src/pages/home/homeComponents/categories/CategoryList/VideoCard.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { useAuthCtx } from "Context/AuthContext";
 import { useHistoryCtx } from "Context/HistoryContext";
+import { useWatchLaterCtx } from "Context/WatchLaterContext";
 
 const AvatarCont = styled.div`
   border-radius: 50%;
@@ -25,6 +26,7 @@ const VideoCard = ({video,sideCard}) => {
     const [dropDown, setDropDown] = useState({state:false,videoId:null});
     const {setPlaylistModal}= usePlaylistCtx();
     const {isLoggedIn} =useAuthCtx();
+    const {addWatchLaterHandler, removeFromWatchLater}=useWatchLaterCtx();
     const {addHistoryHandler}= useHistoryCtx();
   
   return (
@@ -65,7 +67,7 @@ const VideoCard = ({video,sideCard}) => {
                   onClick={()=>{setDropDown({state:false,videoId:null})}}
                 >
                {isLoggedIn?<>
-                  <li>
+                  <li onClick={()=> addWatchLaterHandler(video)}>
                     <i className="fa fa-clock-o" aria-hidden="true"></i>Save To
                     Watch later
                   </li>
@@ -73,7 +75,7 @@ const VideoCard = ({video,sideCard}) => {
                     <i className="fa fa-list-ul" aria-hidden="true"></i>Save To
                     Playlist
                   </li>
-                  <li>
+                  <li onClick={()=> removeFromWatchLater(video._id)}>
                     <i className="fa fa-trash" aria-hidden="true"></i>Remove From
                     Watch later
                   </li></>:<Link to="/login"><li>
