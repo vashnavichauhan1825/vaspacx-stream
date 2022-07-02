256925f4c5f7a06a176873c81ef649e476df2744
vashnavichauhan1825 <vashnavichauhan1@gmail.com>
2022-07-02 23:17:25 +0530

    add create playlist to playlist

--- src/pages/playlist/Playlist.js
+++ src/pages/playlist/Playlist.js
@@ -5,20 +5,30 @@ import Navbar from 'components/Navbar/Navbar'
 import { ToastContainer } from 'react-toastify';
 import BgWrapper from 'components/UI/bgWrapper/BgWrapper';
 import { VaspacxStreamTitle } from 'components/UI/documentTitle/VaspacxStreamTitle';
+import Modal from 'components/UI/modal/Modal';
 const Playlist = () => {
   VaspacxStreamTitle("Playlists")
   const {playlist,removeFromPlaylist}=usePlaylistCtx()
     const playlistLength =playlist.length;
+    const {setPlaylistModal,playlistModal}=usePlaylistCtx();
   return (
    <BgWrapper>
     <Navbar/>
-  <div className='playlist-cont'>{playlistLength?
+  <div className='playlist-cont'>
+  <div className='single-playlist'>
+  <h3 className='create-Playlist-h'>Create Playlist</h3>
+   <div className='playlist-hover'>
+   <i  onClick={() => setPlaylistModal({ state: true, video: {} })} class="fa fa-plus" aria-hidden="true"></i>
+   </div>
+  </div>
+  {playlistLength?
   <>
+  
     {playlist.map((playlist)=>{
      
       return(
         <div key={playlist._id} className='single-playlist'>
-        {console.log(playlist)}
+       
        {playlist.videos.length !==0 && <img src={playlist.videos[0].thumbnail}/>}
         <small>{playlist.description}</small>
         <h3>{playlist.title}</h3>
@@ -31,6 +41,7 @@ const Playlist = () => {
     })}</>: <span className='h1-pri'><h1>your playlist section is empty !</h1></span>}
     </div>
     <ToastContainer/>
+    {playlistModal.state && <Modal/>}
 </BgWrapper>
   )
 }
